fcbf6fdb9c6caa90ade8f7fbd5cb4ff96f99b72a
Ulysse DECOTTIGNIES <ulysse.decottignies@epitech.eu>
2024-09-27 20:32:59 +0900

    [+] Project for Google Vision test set up with a Window, a button to add an image, a picturebox to display said image and another button to extract the text from the image using Google Vision into a second Window

---
+++ ProtoSewt/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace ProtoSewt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.imgUpldBtn = new System.Windows.Forms.Button();
+            this.imgUplddDisp = new System.Windows.Forms.PictureBox();
+            this.txtXtrBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imgUplddDisp)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imgUpldBtn
+            // 
+            this.imgUpldBtn.Location = new System.Drawing.Point(493, 32);
+            this.imgUpldBtn.Name = "imgUpldBtn";
+            this.imgUpldBtn.Size = new System.Drawing.Size(155, 23);
+            this.imgUpldBtn.TabIndex = 0;
+            this.imgUpldBtn.Text = "Ajouter une image";
+            this.imgUpldBtn.UseVisualStyleBackColor = true;
+            this.imgUpldBtn.Click += new System.EventHandler(this.imgUpldBtn_Click);
+            // 
+            // imgUplddDisp
+            // 
+            this.imgUplddDisp.Location = new System.Drawing.Point(493, 61);
+            this.imgUplddDisp.Name = "imgUplddDisp";
+            this.imgUplddDisp.Size = new System.Drawing.Size(155, 113);
+            this.imgUplddDisp.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgUplddDisp.TabIndex = 1;
+            this.imgUplddDisp.TabStop = false;
+            // 
+            // txtXtrBtn
+            // 
+            this.txtXtrBtn.Location = new System.Drawing.Point(654, 93);
+            this.txtXtrBtn.Name = "txtXtrBtn";
+            this.txtXtrBtn.Size = new System.Drawing.Size(130, 42);
+            this.txtXtrBtn.TabIndex = 2;
+            this.txtXtrBtn.Text = "Extraire le texte\n de l\'image";
+            this.txtXtrBtn.UseVisualStyleBackColor = true;
+            this.txtXtrBtn.Click += new System.EventHandler(this.txtXtrBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1197, 519);
+            this.Controls.Add(this.txtXtrBtn);
+            this.Controls.Add(this.imgUplddDisp);
+            this.Controls.Add(this.imgUpldBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imgUplddDisp)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button imgUpldBtn;
+        private System.Windows.Forms.PictureBox imgUplddDisp;
+        private System.Windows.Forms.Button txtXtrBtn;
+    }
+}
+
---
+++ ProtoSewt/Form1.cs
@@ -0,0 +1,101 @@
+﻿using Google.Cloud.Vision.V1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProtoSewt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", @"C:\Users\Odyce\Desktop\Work\prototype-sewt-436908-d52bd6896960.json");
+        }
+
+        private void imgUpldBtn_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp;*.gif";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    Bitmap image = new Bitmap(openFileDialog.FileName);
+
+                    imgUplddDisp.Width = image.Width;
+                    imgUplddDisp.Height = image.Height;
+                    imgUplddDisp.Image = image;
+                    imgUplddDisp.Left = (this.ClientSize.Width - imgUplddDisp.Width) / 2;
+                    txtXtrBtn.Left = (imgUplddDisp.Width + imgUplddDisp.Left) + 10;
+                }
+            }
+        }
+
+        private void txtXtrBtn_Click(object sender, EventArgs e)
+        {
+            if (imgUplddDisp.Image == null)
+            {
+                MessageBox.Show("Please load an image first.");
+                return;
+            }
+
+            // Convert the PictureBox image to a byte array
+            var imageFilePath = ConvertPictureBoxImageToFile();
+
+            // Call Google Vision API to extract text from the image
+            ExtractTextFromImage(imageFilePath);
+        }
+
+        // Function to convert PictureBox image to a file and return the file path
+        private string ConvertPictureBoxImageToFile()
+        {
+            string tempImagePath = Path.Combine(Path.GetTempPath(), "temp_image.png");
+
+            // Save PictureBox image to a temporary file
+            imgUplddDisp.Image.Save(tempImagePath, System.Drawing.Imaging.ImageFormat.Png);
+
+            return tempImagePath;
+        }
+
+        // Function to call Google Vision API to extract text
+        private void ExtractTextFromImage(string imageFilePath)
+        {
+            try
+            {
+                // Create a Vision API client
+                var client = ImageAnnotatorClient.Create();
+
+                // Load the image from the file path
+                var image = Google.Cloud.Vision.V1.Image.FromFile(imageFilePath);
+
+                // Call the Vision API to detect text
+                var response = client.DetectText(image);
+
+                // Display the extracted text
+                string extractedText = string.Empty;
+                foreach (var annotation in response)
+                {
+                    if (annotation.Description != null)
+                    {
+                        extractedText += annotation.Description + "\n";
+                    }
+                }
+
+                // Show the extracted text in a message box
+                MessageBox.Show(extractedText, "Extracted Text");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Error");
+            }
+        }
+    }
+}
